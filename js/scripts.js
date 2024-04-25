e165b81f6d81f444663d8101a905372507ea5189
codesingh <sugi007@yahoo.com>
2024-04-25 23:53:37 +0100

    Changed livestream details to a separate file and uploaded to jsonsilo website

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,16 @@
 jQuery(document).ready(function($) {
-
-  var liveStreams = [{'channelid': 'UCMqlz5xjRN29s51_Jddjo1Q', 'name':'Ontario Khalsa Darbar Official'},
-                    {'channelid': 'UCdeALIwTSRHl5WtQvmAlSmw', 'name':'SGSSSouthall'},
-                    {'channelid': 'UCUvTNT7gI4YgtbPzzemvQmA', 'name':'Gurdwara Bebe Nanaki ji'},
-                    {'channelid': 'UCsRllxbDm0oxskk8VI4CmBA', 'name':'Sri Guru Singh Sabha Malton Official'},
-                    {'channelid': 'UCloMqFkKMqIp2Jhp1A4_C7w', 'name': 'Gurdwara Guru Nanak Dev Ji Bradford'},
-                    {'channelid': 'UCA1Jqo-WXVuMgs4WcD5f5Yw', 'name': 'Gurdwara Bangla Sahib Ji'},
-                    {'channelid': 'UCYn6UEtQ771a_OWSiNBoG8w', 'name': 'Shri Harmandir Sahib'},
-                    {'channelid': 'UC5Ls0yJbNABQn8HeARivOSQ', 'name': 'Dashmesh Culture Centre Calgary'},
-                    {'channelid': 'UCHahhxFo1hUyO39Yh5cixRQ', 'name': 'Sher E Punjab Gurbani'},
-                    {'channelid': 'UCNYMuETXWtm6Nh1R4wGESIQ', 'name': 'Dukh Nivaran Sahib Surrey'},
-                    {'channelid': 'UC3ZsJ-3_rjgsAZXbjTfXZnA', 'name': 'Sri Guru Gobind Singh Gurdwara Manchester'},
-                    {'channelid': 'UCnIaOhevdG-l_XcVqDBlC5Q', 'name': 'Gurdwara Yuba City-Gurbani Live USA'},
-                    {'channelid': 'UCfNPTSmJYHAtBnTUdGfPR7A', 'name': 'Gurdwara Sahib Glenwood, Sydney'},
-                    {'channelid': 'UC17LEWEX-U0H5PdbPtmjFfA', 'name': 'Guru Nanak Sikh Gurdwara, Surrey'},
-                    {'channelid': 'UC5yAw8-hjqLz8gqIbN5vVGw', 'name': 'Gurdwara Dukh Niwaran Sahib Ludhiana'},
-                  ]; 
-
   var inc = 0;
 
-  document.getElementById('channelname').innerText = liveStreams[inc].name;
-  document.getElementById('gurdwaraliveplayer').src = "https://www.youtube.com/embed/live_stream?channel=" + liveStreams[inc].channelid + "&autoplay=1&muted=1";
+  let liveStreams;
+  fetch('https://api.jsonsilo.com/public/cc00cc1e-47e2-41bf-8add-31ddd053f5b9').then(
+    function(u){ return u.json();}
+  ).then(
+    function(json){
+      liveStreams = json;
+      document.getElementById('channelname').innerText = liveStreams[inc].name;
+      document.getElementById('gurdwaraliveplayer').src = "https://www.youtube.com/embed/" + liveStreams[inc].channelid + "?autoplay=1&muted=1";
+    }
+  )
 
   $('button#nextvid').click(function() {
     nextVid(true);
@@ -43,9 +33,8 @@ jQuery(document).ready(function($) {
     if(isBtnPress){
       inc= ++inc % liveStreams.length
       document.getElementById('channelname').innerText = liveStreams[inc].name;
-      document.getElementById('gurdwaraliveplayer').src = "https://www.youtube.com/embed/live_stream?channel=" + liveStreams[inc].channelid + "&autoplay=1&muted=1";
-      console.log("https://www.youtube.com/embed/live_stream?channel=" + liveStreams[inc].channelid + "&autoplay=1&muted=1");
-
+      document.getElementById('gurdwaraliveplayer').src = "https://www.youtube.com/embed/" + liveStreams[inc].videoid + "?autoplay=1&muted=1&enablejsapi=1&version=3&origin=codesingh.github.io";
+      console.log("https://www.youtube.com/embed/" + liveStreams[inc].videoid + "?autoplay=1&muted=1&enablejsapi=1&version=3&origin=codesingh.github.io");
     }
     setTimeout(function(){$('.videooverlay').removeClass('videonoise');}, 1000);
   }
